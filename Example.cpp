5409c286d9b1e0906e7939fa407544450d882576
Furkan Üzümcü <furkanuzumcu@gmail.com>
2014-03-03 17:06:10 +0200

    Create Example.cpp
    An example to show how it works...

---
+++ Example.cpp
@@ -0,0 +1,55 @@
+#include "SFML/Graphics.hpp"
+#include "ResourceHolder.h"
+
+enum Textures {
+    A,
+    B,
+    C,
+    D
+};
+
+int main()
+{
+    // Create a stream and open a file that is inside "MyWonderfulArchive.7z".
+    PhysFsStream wonderfullStream("B.zip");//Or use PhysFsStream::addSearchPath(const char *compressedFilePath)
+    wonderfullStream.open("A.jpg");//Open a file inside of the zip file
+    ResourceHolder<sf::Texture, Textures> rc;
+    //You can use only ResourceHolder::load function if you don't want to load assets from stream
+    rc.loadFromStream(Textures::A, wonderfullStream);
+    wonderfullStream.open("B.jpg");
+    rc.loadFromStream(Textures::B, wonderfullStream);
+    wonderfullStream.open("C.jpg");
+    rc.loadFromStream(Textures::C, wonderfullStream);
+    wonderfullStream.open("D.png");
+    rc.loadFromStream(Textures::D, wonderfullStream);
+    sf::Sprite sprite;
+    // Create a window and display the sprite
+    sf::RenderWindow app(sf::VideoMode(1368, 768), "PhysFS", sf::Style::Default);
+    app.setFramerateLimit(60);
+    while(app.isOpen())
+    {
+        sf::Event event;
+        while (app.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                app.close();
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+                app.close();
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+                sprite.setTexture(rc.get(Textures::A));
+            }
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::B)) {
+                sprite.setTexture(rc.get(Textures::B));
+            }
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::C)) {
+                sprite.setTexture(rc.get(Textures::C));
+            }
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+                sprite.setTexture(rc.get(Textures::D));
+            }
+        }
+        app.clear();
+        app.draw(sprite);
+        app.display();
+    }
+    return 0;
+}
